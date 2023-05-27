c8e649e436e47af5c95b14b96e725ca78e14b918
dibil228of <85364286+dibil228of@users.noreply.github.com>
2023-05-27 20:31:26 +0400

    Add files via upload

--- js/themes.js
+++ js/themes.js
@@ -1,39 +1,32 @@
 function wal1() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/1.jpg);";
 };
 
-function wal2
-() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+function wal2() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/2.jpg);";
 };
 
-function wal3
-() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+function wal3() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/3.jpg);";
 };
 
-function wal4
-() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+function wal4() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/4.jpg);";
 };
 
-function wal5
-() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+function wal5() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/5.jpg);";
 };
 
-function wal6
-() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+function wal6() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/6.jpg);";
 };
 
-function wal7
-() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+function wal7() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/7.jpg);";
 };
 
-function wal8
-() {
-	alert("Это пока-что не работает, ждите новое обновление Блоп ОС Мобайл!");
+function wal8() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/8.jpg);";
 };
 
