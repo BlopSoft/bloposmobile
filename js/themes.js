320b6130916982e71687f2766c0ee7db089f4daf
dibil228of <85364286+dibil228of@users.noreply.github.com>
2023-11-01 12:16:07 +0400

    Add files via upload

--- js/themes.js
+++ js/themes.js
@@ -7,42 +7,6 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
 }); 
 
-function wal1() {
-	document.cookie = "wall=1";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
-};
-
-function wal2() {
-	document.cookie = "wall=2";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
-};
-
-function wal3() {
-	document.cookie = "wall=3";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
-};
-
-function wal4() {
-	document.cookie = "wall=4";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
-};
-
-function wal5() {
-	document.cookie = "wall=5";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
-};
-
-function wal6() {
-	document.cookie = "wall=6";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
-};
-
-function wal7() {
-	document.cookie = "wall=7";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
-};
-
-function wal8() {
-	document.cookie = "wall=8";
-	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/" + cookieValue + ".jpg);";
+function wal(name) {
+	document.cookie = "wall=" + name;
 };
