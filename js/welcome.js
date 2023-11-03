ee282b211e772bde559f9ddb5d6985bcf5d3341e
dibil228of <85364286+dibil228of@users.noreply.github.com>
2023-11-03 20:19:38 +0400

    Update welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -4,8 +4,8 @@ var cookieValue = document.cookie.replace(
 );
 
 function start() {
-    document.cookie = "start=1";
-    document.cookie = "wall=1";
+    document.cookie = 'start=1; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+    document.cookie = 'wall=1; expires=Fri, 31 Dec 9999 23:59:59 GMT';
     document.cookie = 'cookiesAccepted=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
     location.replace("desktop.html");
 };
